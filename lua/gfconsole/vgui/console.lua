dc9bf5f9cc0201e7299d8af8f20d09c41bfeea9e
Aleksandrs Filipovskis <tochnonement@gmail.com>
2021-03-13 01:20:40 +0200

    Now frame can be resized

--- lua/gfconsole/vgui/console.lua
+++ lua/gfconsole/vgui/console.lua
@@ -30,4 +30,33 @@ function PANEL:Paint(w, h)
     surface.DrawRect(0, 0, w, h)
 end
 
+function PANEL:Think()
+    self:ResizeController()
+end
+
+-- Custom methods
+
+function PANEL:ResizeController()
+    local x, y = self:GetRelativeToCursor()
+    local w, h = self:GetSize()
+
+    if x > (w - 8) and y > (h - 8) then
+        self.resizable = input.IsMouseDown(MOUSE_LEFT)
+        self:SetCursor("sizenwse")
+    else
+        self:SetCursor("cursor")
+    end
+
+    if self.resizable then
+        self:SetSize(x, y)
+    end
+end
+
+function PANEL:GetRelativeToCursor()
+    local x, y = self:LocalToScreen(0, 0)
+    local x2, y2 = input.GetCursorPos()
+
+    return (x2 - x), (y2 - y)
+end
+
 vgui.Register("GFConsole", PANEL, "EditablePanel")
