4450eeed97896768d1bae74e2b778caa64c06ead
tochnonement <tochnonement@gmail.com>
2022-04-27 22:00:26 +0300

    better scaling for fonts

--- lua/gfconsole/core/cl_fonts.lua
+++ lua/gfconsole/core/cl_fonts.lua
@@ -7,28 +7,32 @@ Email: tochonement@gmail.com
 
 --]]
 
+local function scale(int)
+    return math.ceil(int / 900 * ScrH())
+end
+
 surface.CreateFont("gfconsole.Title", {
     font = "Roboto Medium",
-    size = 18,
+    size = scale(18),
     extended = true
 })
 
 surface.CreateFont("gfconsole.Button", {
     font = "Roboto",
-    size = 16,
+    size = scale(16),
     extended = true
 })
 
 surface.CreateFont("gfconsole.Text", {
     font = "Roboto",
     shadow = true,
-    size = 16,
+    size = scale(16),
     extended = true
 })
 
 surface.CreateFont("gfconsole.Text.underline", {
     font = "Roboto",
-    size = 16,
+    size = scale(16),
     shadow = true,
     extended = true,
     italic = true
